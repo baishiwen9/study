434f357d107d16d84c4dae6dae8c87c702278cea
baishiwen <baishiwen@babytree-inc.com>
2020-05-12 19:30:50 +0800

    feat previewImage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import BST from './算法/Tree.js';
 import EventEmitter from './功能/EventEmitter';
 import LazyLoad from './功能/ImageLazyLoad';
+import PreviewImage from './功能/PreviewImage';
+
+
 
 
 const Tools = {
     BST,
     EventEmitter,
     LazyLoad,
+    PreviewImage,
 }
 
 export default Tools;
---
+++ src/功能/PreviewImage.js
@@ -0,0 +1,42 @@
+
+/**
+ * 预览图片
+ */
+
+
+class PreviewImage {
+    constructor() {
+        this.init();
+    }
+    init() {
+        document.addEventListener('click', (e) => {
+            if (e.target.nodeName === 'IMG') {
+                const src = e.target.getAttribute('src');
+                window.console.log('当前点击图片的src', src);
+                this.PreviewModel(src, e);
+            }
+        }, false);
+    }
+    PreviewModel(src, e) {
+        e && e.stopPropagation();
+        const html = `
+            <div style="position:fixed; width: 100%; height:100%; background: #333; z-index:999;top: 0;left:0;">
+                <img src=${src} style="width: 100%;height: 100%;object-fit: contain;" alt="" />
+            </div>
+        `;
+        const node = document.createElement('div');
+        node.setAttribute('id', 'bbt-previewImage');
+        node.innerHTML = html;
+        document.body.appendChild(node);
+        node.addEventListener('click', this.closeModel, false);
+    }
+    closeModel(e) {
+        e && e.stopPropagation();
+        const node = document.getElementById('bbt-previewImage');
+        if (node) {
+            document.body.removeChild(node);
+        }
+    }
+}
+
+export default PreviewImage;
